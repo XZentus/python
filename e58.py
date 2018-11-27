29cf31abe0c5699886c15edc01874cd9cb537c63
XZentus <z_xmn@mail.ru>
2018-11-27 17:51:55 +0300

    Update e58.py

--- e58.py
+++ e58.py
@@ -6,33 +6,48 @@ Created on Tue Nov 27 14:11:33 2018
 """
 
 from fractions import Fraction
-from sieve import Sieve
+
+from math import sqrt
 
 def dr(n):
     return n*n
 
 def dl(n):
-    return dr(n) - n + 1
+    return n*n - n + 1
 
 def ul(n):
-    return dr(n) - 2*n + 2
+    return n*n - 2*n + 2
 
 def ur(n):
-    return dr(n) - 3*n + 3
+    return n*n - 3*n + 3
 
-def check_sq(size, erast):
-    return (sum(erast.is_prime(f(size)) for f in [dr, dl, ul, ur]), 4)
-
-def solve(sieve_limit = 500000000):
-    e = Sieve(sieve_limit*2)
+def is_prime_simple(n):
+    if n % 2 == 0:
+        return false
+        
+    if n < 10:
+        if n in [3, 5, 7]:
+            return True
+        return False
     
+    for i in range(3, int(sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def check_sq(size):
+    return sum(is_prime_simple(f(size)) for f in [dr, dl, ul, ur])
+
+def solve():
+    limit = 50000
     primes, nums = 0, 1
     i = 3
     while True:
-        ps, ns = check_sq(i, e)
-        primes, nums = primes + ps, nums + ns
+        ps = check_sq(i)
+        primes, nums = primes + ps, nums + 4
 
-        if i*i >= sieve_limit:
+        if i >= limit:
             print('OVERFLOW: i={}: {}/{} => {}'.format(i, primes, nums, primes/nums))
             return None
         
@@ -42,3 +57,4 @@ def solve(sieve_limit = 500000000):
         # print('i={} => {}/{} => {}'.format(i, primes, nums, primes/nums))
         # input()
         i += 2
+    return primes, nums, i
